dceb46746a5c79a77079048e7cae84da03fe28b1
Martin Björkström <martin.bjorkstrom@gmail.com>
2021-05-09 23:02:51 +0300

    Cache documents.

--- src/Statiq.Docs/Pipelines/Api.cs
+++ src/Statiq.Docs/Pipelines/Api.cs
@@ -31,40 +31,41 @@ namespace Statiq.Docs.Pipelines
                         || ctx.Settings.GetList<string>(DocsKeys.ProjectFiles)?.Count > 0
                         || ctx.Settings.GetList<string>(DocsKeys.SolutionFiles)?.Count > 0),
                     new ConcatDocuments(nameof(Code)),
-                    new ExecuteConfig(Config.FromContext(ctx =>
-                        new AnalyzeCSharp()
-                            .WhereNamespaces(ctx.Settings.GetBool(DocsKeys.IncludeGlobalNamespace))
-                            .WherePublic()
-                            .WithCssClasses("code", "cs")
-                            .WithDestinationPrefix(ctx.GetPath(DocsKeys.ApiPath))
-                            .WithAssemblies(Config.FromContext<IEnumerable<string>>(ctx => ctx.GetList<string>(DocsKeys.AssemblyFiles)))
-                            .WithProjects(Config.FromContext<IEnumerable<string>>(ctx => ctx.GetList<string>(DocsKeys.ProjectFiles)))
-                            .WithSolutions(Config.FromContext<IEnumerable<string>>(ctx => ctx.GetList<string>(DocsKeys.SolutionFiles)))
-                            .WithAssemblySymbols()
-                            .WithImplicitInheritDoc(ctx.GetBool(DocsKeys.ImplicitInheritDoc)))),
-                    new ExecuteConfig(Config.FromDocument((doc, ctx) =>
-                    {
-                        // Calculate a type name to link lookup for auto linking
-                        string name = null;
-                        string kind = doc.GetString(CodeAnalysisKeys.Kind);
-                        if (kind == "NamedType")
+                    new CacheDocuments(
+                        new ExecuteConfig(Config.FromContext(ctx =>
+                            new AnalyzeCSharp()
+                                .WhereNamespaces(ctx.Settings.GetBool(DocsKeys.IncludeGlobalNamespace))
+                                .WherePublic()
+                                .WithCssClasses("code", "cs")
+                                .WithDestinationPrefix(ctx.GetPath(DocsKeys.ApiPath))
+                                .WithAssemblies(Config.FromContext<IEnumerable<string>>(ctx => ctx.GetList<string>(DocsKeys.AssemblyFiles)))
+                                .WithProjects(Config.FromContext<IEnumerable<string>>(ctx => ctx.GetList<string>(DocsKeys.ProjectFiles)))
+                                .WithSolutions(Config.FromContext<IEnumerable<string>>(ctx => ctx.GetList<string>(DocsKeys.SolutionFiles)))
+                                .WithAssemblySymbols()
+                                .WithImplicitInheritDoc(ctx.GetBool(DocsKeys.ImplicitInheritDoc)))),
+                        new ExecuteConfig(Config.FromDocument((doc, ctx) =>
                         {
-                            name = doc.GetString(CodeAnalysisKeys.DisplayName);
-                        }
-                        else if (kind == "Property" || kind == "Method")
-                        {
-                            IDocument containingType = doc.GetDocument(CodeAnalysisKeys.ContainingType);
-                            if (containingType != null)
+                            // Calculate a type name to link lookup for auto linking
+                            string name = null;
+                            string kind = doc.GetString(CodeAnalysisKeys.Kind);
+                            if (kind == "NamedType")
                             {
-                                name = $"{containingType.GetString(CodeAnalysisKeys.DisplayName)}.{doc.GetString(CodeAnalysisKeys.DisplayName)}";
+                                name = doc.GetString(CodeAnalysisKeys.DisplayName);
                             }
-                        }
-                        if (name != null)
-                        {
-                            TypeNameLinks typeNameLinks = ctx.GetService<TypeNameLinks>();
-                            typeNameLinks.Links.AddOrUpdate(WebUtility.HtmlEncode(name), ctx.GetLink(doc), (x, y) => string.Empty);
-                        }
-                    }))));
+                            else if (kind == "Property" || kind == "Method")
+                            {
+                                IDocument containingType = doc.GetDocument(CodeAnalysisKeys.ContainingType);
+                                if (containingType != null)
+                                {
+                                    name = $"{containingType.GetString(CodeAnalysisKeys.DisplayName)}.{doc.GetString(CodeAnalysisKeys.DisplayName)}";
+                                }
+                            }
+                            if (name != null)
+                            {
+                                TypeNameLinks typeNameLinks = ctx.GetService<TypeNameLinks>();
+                                typeNameLinks.Links.AddOrUpdate(WebUtility.HtmlEncode(name), ctx.GetLink(doc), (x, y) => string.Empty);
+                            }
+                    })))));
         }
     }
 }
