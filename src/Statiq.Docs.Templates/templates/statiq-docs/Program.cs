a2e171c7d7e9bd795866eb62f4ba023ac0a9dbe9
Dave Glick <dave@daveaglick.com>
2022-02-15 14:01:56 -0500

    Added a Statiq Docs dotnet new template

---
+++ src/Statiq.Docs.Templates/templates/statiq-docs/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using Statiq.App;
+using Statiq.Docs;
+
+namespace StatiqDocs
+{
+    public class Program
+    {
+        public static async Task<int> Main(string[] args) =>
+            await Bootstrapper
+                .Factory
+                .CreateDocs(args)
+                .RunAsync();
+    }
+}
